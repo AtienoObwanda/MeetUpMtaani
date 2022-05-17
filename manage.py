78e9b79edba5fd2a921787bd8ee2cb1a7e78606c
atienoobwanda <atienoobwanda@gmail.com>
2022-05-17 20:22:45 +0300

    Updating manage

--- manage.py
+++ manage.py
@@ -1,7 +1,32 @@
-from app import create_app
+from flask_script import Manager, Server
+from  flask_migrate import Migrate, MigrateCommand
 
-app = create_app('development')
+from app import create_app,db
 
+
+from app.models import User, Admin,Review,Reservation,Deals
+
+
+# App instance -> creating app instance
+app = create_app('production')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app,db=db,User=User, Admin=Admin, Reservation=Reservation, Review=Review,Deals= Deals)
+
+
+migrate = Migrate(app,db)
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
