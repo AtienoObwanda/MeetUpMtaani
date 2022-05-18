4aca0fe9e847172d8f6b217e52c35ad9c6c1cd2e
atienoobwanda <atienoobwanda@gmail.com>
2022-05-18 04:54:10 +0300

    adding URI

---
+++ app/adminAuth/views.py
@@ -0,0 +1,4 @@
+SECRET_KEY = '907a65b00fc0162681e064681501fc4'
+DATABASE_URL = 'postgresql+psycopg2://atieno:mishi@localhost/meetup'
+MAIL_USERNAME = 'splashpitch@gmail.com'
+MAIL_PASSWORD = 'afSG7AyE123clear'
--- config.py
+++ config.py
@@ -1,7 +1,26 @@
 import os
 
 class Config:
-    pass
+    '''
+    Parent config class
+    '''
+    SECRET_KEY =os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
+    # Mail confugurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS=True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SUBJECT_PREFIX = 'PITCH SPLASH!'
+    SENDER_EMAIL = 'splashpitch@gmail.com'
+
+
+    @staticmethod
+    def init_app(app):
+        pass
+
 
 
 class ProdConfig(Config):
@@ -9,7 +28,12 @@ class ProdConfig(Config):
 
 
 class DevConfig(Config):
-    DEBUG = True
+    '''
+    development class
+    '''
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+DEBUG = True
 
 config_options = {
 'development':DevConfig,
